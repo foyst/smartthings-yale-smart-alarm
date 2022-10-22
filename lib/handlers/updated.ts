68771f78b2b3d11c4f35942f04a7426a4a0f0c51
Ben Foster <benfoster88@hotmail.com>
2022-10-22 22:00:31 +0100

    WIP Yale OAuth Integration

--- lib/handlers/updated.ts
+++ lib/handlers/updated.ts
@@ -1,7 +1,13 @@
 import { SmartAppContext } from "@smartthings/smartapp"
+import yaleClient from "../yaleClient"
 
 export default function (context: SmartAppContext, updateData: any) {
     
     console.log("Yale SR SmartApp being installed")
+    const yaleEmail = context.configStringValue('email')
+    const yalePassword = context.configStringValue('password')
+    yaleClient.loginWithUsernameAndPassword(yaleEmail, yalePassword)
+    const installedAppId = context.api.config.installedAppId
+
     context.api.subscriptions.subscribeToSecuritySystem('securityModeChangeHandler')
 }
