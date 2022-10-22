d97b1bca6797d0193d0d2895f4c7b416af8993bf
Ben Foster <benfoster88@hotmail.com>
2022-10-22 21:34:30 +0100

    WIP Yale OAuth Integration

--- constants.ts
+++ constants.ts
@@ -1,3 +1,8 @@
 export const AppSettings = { 
     yaleAuthToken: "VnVWWDZYVjlXSUNzVHJhcUVpdVNCUHBwZ3ZPakxUeXNsRU1LUHBjdTpkd3RPbE15WEtENUJ5ZW1GWHV0am55eGhrc0U3V0ZFY2p0dFcyOXRaSWNuWHlSWHFsWVBEZ1BSZE1xczF4R3VwVTlxa1o4UE5ubGlQanY5Z2hBZFFtMHpsM0h4V3dlS0ZBcGZzakpMcW1GMm1HR1lXRlpad01MRkw3MGR0bmNndQ==", 
   } as const;
+
+ export interface Tokens {
+    accessToken: string
+    refreshToken: string
+}
