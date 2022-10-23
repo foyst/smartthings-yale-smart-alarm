cfda5ac81cd75ab3adfdbb70a8584e98a08babf9
Ben Foster <benfoster88@hotmail.com>
2022-10-23 22:14:49 +0100

    Finished initial implementation of OAuth token management

--- lib/handlers/updated.ts
+++ lib/handlers/updated.ts
@@ -1,13 +1,14 @@
 import { SmartAppContext } from "@smartthings/smartapp"
-import yaleClient from "../yaleClient"
+import { createYaleClient } from "../yaleClient"
 
 export default function (context: SmartAppContext, updateData: any) {
     
     console.log("Yale SR SmartApp being installed")
     const yaleEmail = context.configStringValue('email')
     const yalePassword = context.configStringValue('password')
+
+    const yaleClient = createYaleClient(context);
     yaleClient.loginWithUsernameAndPassword(yaleEmail, yalePassword)
-    const installedAppId = context.api.config.installedAppId
 
     context.api.subscriptions.subscribeToSecuritySystem('securityModeChangeHandler')
 }
--- lib/yaleTokenStore.ts
+++ lib/yaleTokenStore.ts
@@ -1,4 +1,4 @@
-import { DynamoDBClient, GetItemCommand, GetItemCommandInput, PutItemCommand, PutItemCommandInput } from "@aws-sdk/client-dynamodb";
+import { DeleteItemCommand, DeleteItemCommandInput, DynamoDBClient, GetItemCommand, GetItemCommandInput, PutItemCommand, PutItemCommandInput } from "@aws-sdk/client-dynamodb";
 import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
 import { Tokens } from "../constants";
 
@@ -6,23 +6,24 @@ const client = new DynamoDBClient({});
 
 export default {
 
-    async get(installedAppId: String): Promise<Tokens> {
+    async get(installedAppId: string): Promise<Tokens> {
 
-    const params: GetItemCommandInput = {
-        TableName: "SmartThingsYaleTokens",
-        Key: marshall(installedAppId),
+        const params: GetItemCommandInput = {
+            TableName: "SmartThingsYaleTokens",
+            Key: marshall({ installedAppId: installedAppId }),
         };
 
-        const results = await client.send(new GetItemCommand(params));
+        const getItemCommand = new GetItemCommand(params);
+        const results = await client.send(getItemCommand);
         const tokens = unmarshall(results.Item || {})
         return {
             accessToken: tokens.accessToken,
             refreshToken: tokens.accessToken
         }
     },
-    
-    async put(installedAppId: String, tokens: Tokens) {
-    
+
+    async put(installedAppId: string, tokens: Tokens) {
+
         const params: PutItemCommandInput = {
             TableName: "SmartThingsYaleTokens",
             Item: marshall({
@@ -32,5 +33,14 @@ export default {
             })
         };
         await client.send(new PutItemCommand(params));
+    },
+
+    async delete(installedAppId: string) {
+
+        const params: DeleteItemCommandInput = {
+            TableName: "SmartThingsYaleTokens",
+            Key: marshall({ installedAppId: installedAppId }),
+        };
+        await client.send(new DeleteItemCommand(params))
     }
 }
